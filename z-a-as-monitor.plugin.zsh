56e3b05280a538e3757e9f7bbf542f3def6e79df
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-18 02:47:01 +0100

    Rename the handler from :za-* to →za-* to allow using on Windows

--- z-a-as-monitor.plugin.zsh
+++ z-a-as-monitor.plugin.zsh
@@ -7,15 +7,15 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz :za-as-monitor-preinit-handler
+autoload -Uz →za-as-monitor-preinit-handler
 
 # An empty stub to fill the help handler fields
-:za-as-monitor-help-null-handler() { :; }
+→za-as-monitor-help-null-handler() { :; }
 
 @zinit-register-annex "z-a-as-monitor" \
     hook:preinit \
-    :za-as-monitor-preinit-handler \
-    :za-as-monitor-help-null-handler \
+    →za-as-monitor-preinit-handler \
+    →za-as-monitor-help-null-handler \
     "dlink''|.monitor''" # The ice conflict with dl'' from z-a-patch-dl is being handled
                        # in the other annex
 

bb5ee1e44dfaddb722affc95c51f404884a628e9
Salvydas Lukosius <sall@w-ss.io>
2021-11-14 21:36:44 +0000

    Update z-a-readurl.plugin.zsh

--- z-a-readurl.plugin.zsh
+++ z-a-readurl.plugin.zsh
@@ -1,10 +1,8 @@
-# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-
 # Copyright (c) 2019-2020 Sebastian Gniazdowski
 # License MIT
 
 # According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://z-shell.github.io/ZSH-TOP-100/Zsh-Plugin-Standard.html
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -18,7 +16,4 @@ autoload -Uz →za-readurl-preinit-handler
     hook:preinit-10 \
     →za-readurl-preinit-handler \
     →za-readurl-help-null-handler \
-    "dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled
-                       # in the other annex
-
-# vim:ft=zsh:tw=80:sw=4:sts=4:et
+    "dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex

f4fbd7261d0d2d857973dcfa5bb3b14f426c5dcd
Salvydas Lukosius <sall@w-ss.io>
2021-12-06 08:45:05 +0000

    modified:   .editorconfig 	modified:   .gitignore 	renamed:    README.md -> docs/README.md 	modified:   z-a-readurl.plugin.zsh 	new file:   za-readurl-preinit-handler 	deleted:    "\342\206\222za-readurl-preinit-handler"

--- z-a-readurl.plugin.zsh
+++ z-a-readurl.plugin.zsh
@@ -7,13 +7,11 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz →za-readurl-preinit-handler
+autoload -Uz za-readurl-preinit-handler
 
 # An empty stub to fill the help handler fields
-→za-readurl-help-null-handler() { :; }
+za-readurl-help-null-handler() { :; }
 
-@zinit-register-annex "z-a-readurl" \
-    hook:preinit-10 \
-    →za-readurl-preinit-handler \
-    →za-readurl-help-null-handler \
-    "dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex
+@zinit-register-annex "z-a-readurl" hook:preinit-10 \
+za-readurl-preinit-handler za-readurl-help-null-handler \
+"dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex

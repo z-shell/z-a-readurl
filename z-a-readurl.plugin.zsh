1e3c948fd3400195a805aff3e16b4c9820bb19c8
Salvydas Lukosius <sall@w-ss.io>
2022-04-06 12:53:14 +0100

    refactor(workspace): ⚡️ Refactor to up to date - zsh plugin standart

--- z-a-readurl.plugin.zsh
+++ z-a-readurl.plugin.zsh
@@ -2,15 +2,22 @@
 # Copyright (c) 2021 Z-Shell ZI Contributors
 #
 # According to the Zsh Plugin Standard:
-# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard#1-standardized-0-handling
-
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz za-readurl-preinit-handler
+# https://z.digitalclouds.dev/community/zsh_plugin_standard/#funtions-directory
+if [[ $PMSPEC != *f* ]] {
+    fpath+=( "${0:h}/functions" )
+}
+
+autoload -Uz →za-readurl-preinit-handler
 
 # An empty stub to fill the help handler fields
-za-readurl-help-null-handler() { :; }
+→za-readurl-help-null-handler() { :; }
 
 @zi-register-annex "z-a-readurl" hook:preinit-10 \
-za-readurl-preinit-handler za-readurl-help-null-handler "dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex
+  →za-readurl-preinit-handler \
+  →za-readurl-help-null-handler "dlink''|.readurl''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex
+
+# vim: ft=zsh sw=2 ts=2 et
